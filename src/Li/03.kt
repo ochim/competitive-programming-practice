747d1fc046afa8334e3e2ddd0c5ad7b5911bfc90
ochi <mune.ochi@gmail.com>
2021-11-09 23:09:31 +0900

    catch exception

--- src/Li/03.kt
+++ src/Li/03.kt
@@ -3,22 +3,37 @@
 */
 fun solutionL3(A: Array<String>): IntArray {
     // write your code in Kotlin 1.3.11 (Linux)
-    val sample = Sample()
-    for (i in A.indices) {
-        val readList = A[i].split(" ")
-        when (readList[0]) {
-            "exit" -> { break }
-            "add" -> { sample.add(readList[1].toInt(), readList[2].toInt()) }
-            "get" -> { sample.get(readList[1].toInt()) }
-            "remove" -> { sample.remove(readList[1].toInt()) }
-            "evict" -> { sample.evict() }
-            else -> {}
+    val custom = Custom()
+    try {
+        for (i in A.indices) {
+            val readList = A[i].split(" ")
+            when (readList[0]) {
+                "exit" -> {
+                    break
+                }
+                "add" -> {
+                    custom.add(readList[1].toInt(), readList[2].toInt())
+                }
+                "get" -> {
+                    custom.get(readList[1].toInt())
+                }
+                "remove" -> {
+                    custom.remove(readList[1].toInt())
+                }
+                "evict" -> {
+                    custom.evict()
+                }
+                else -> {
+                    break
+                }
+            }
         }
+    } catch(e: Exception) {
     }
-    return sample.output()
+    return custom.output()
 }
 
-class Sample {
+class Custom {
     var inputMap = mutableMapOf<Int, Int>()
     var outputList = mutableListOf<Int>()
     var usedKeys = mutableListOf<Int>()
@@ -59,6 +74,9 @@ fun main(args: Array<String>) {
     repeat(1) {
         println(
             solutionL3(arrayOf("add 1 2", "get 1", "exit")).forEach { print("$it,") }
+        )
+        println(
+            solutionL3(arrayOf("add 1 2", "get", "exit"))
         )
     }
 }
