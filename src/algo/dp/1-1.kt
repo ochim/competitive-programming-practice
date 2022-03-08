50666d71f072eb5c92f4e0d80df1b99a4de45295
ochi <mune.ochi@gmail.com>
2022-03-08 21:05:06 +0900

    algo method 動的計画法

---
+++ src/algo/dp/1-1.kt
@@ -0,0 +1,15 @@
+package algo.dp
+
+fun main() {
+    val A = readLine()!!.split(" ").map{ it.toInt() }
+    val N = A[0]
+    val X = A[1]
+    val Y = A[2]
+
+    val l = mutableListOf(X, Y)
+    for (i in 2 until N) {
+        l.add((l[i-2] + l[i-1]) % 100)
+    }
+
+    println(l[N-1])
+}
---
+++ src/algo/dp/1-2.kt
@@ -0,0 +1,16 @@
+package algo.dp
+
+import kotlin.math.min
+
+fun main() {
+    val N = readLine()!!.toInt()
+    val A = readLine()!!.split(" ").map{ it.toInt() }
+
+    val T = mutableListOf(0, A[1])
+    for (i in 2 until N) {
+        val t = min(T[i-1] + A[i], T[i-2] + A[i] * 2)
+        T.add(t)
+    }
+
+    println(T[N-1])
+}
